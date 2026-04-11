6669f7b3a0a4790be4adc6a3ea0e075b1f6ede59
Christian Lorenzen <christian.lorenzen119@gmail.com>
2026-04-10 23:29:00 -0400

    add library download to build as needed

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+quill_url="https://github.com/slab/quill.git"
+quill_dest="src/include/quill"
+
+assimp_url="https://github.com/assimp/assimp.git"
+assimp_dest="src/include/assimp"
 
 # Exit if any command fails
 set -e
+
+if [ ! -d "$quill_dest" ]; then
+    echo "Cloning quill..."
+    git clone --depth 1 $quill_url $quill_dest
+else
+    echo "Quill already exists, skipping clone."
+fi
+
+if [ ! -d "$assimp_dest" ]; then
+    echo "Cloning assimp..."
+    git clone --depth 1 $assimp_url $assimp_dest
+else
+    echo "Assimp already exists, skipping clone."
+fi
 
 cmake -S . -B build
 cmake --build build -j
